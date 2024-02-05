e16a030d62bcefc0af43b85a0e2e385479271c2d
AmrithaAngelin <amrithama03@gmail.com>
2024-02-05 12:20:05 +0530

    second_commit

--- Data/data.py
+++ Data/data.py
@@ -12,6 +12,10 @@ TC01_Portfolio_Description2 ='TC01_portfolio_description2'
 
 
 
-TC02_Category_name = 'TC02_Cateory'
+# Adding Category to Finance Configuration
+TC02_Category_name = 'TC02_Category'
 TC02_Category_Description = 'TC02_Category_description'
+TC02_Invalid_Category = 'TC002_Category'
+TC02_Category_name_new = 'TC02_Cateorynew'
+TC02_Category_Description_new = 'TC02_Category_descriptionnew'
 
