9e50a1914b6b25be222194edea37f7f79cfb26ef
HarishArchimedis <info.harish.p@gmail.com>
2024-02-05 16:46:32 +0530

    Deleted All

--- Auto.bat
+++
@@ -1,6 +0,0 @@
-@REM @REM del /q "output\pabot_results"
-
-@REM pip install -r .\requirements.txt
-Set Tags=Test
-Set Thread=5
-pabot --suitesfrom .\Testcase\ -st --processes %Thread% -d .\output\ -i %Tags% .
